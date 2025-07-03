09b240f558f6fe37bc0b7e2f8d2f2abf4196d9e4
shiv <shivamlal2412@gmail.com>
2025-07-03 21:29:20 +0530

    minor adjustments

--- frontend/src/components/NoChatSelected.jsx
+++ frontend/src/components/NoChatSelected.jsx
@@ -17,9 +17,9 @@ const NoChatSelected = () => {
         </div>
 
         {/* Welcome Text */}
-        <h2 className="text-2xl font-bold">Welcome to Chatty!</h2>
+        <h2 className="text-2xl font-bold">Welcome to GupShup!</h2>
         <p className="text-base-content/60">
-          Select a conversation from the sidebar to start chatting
+          Select a conversation to your loved ones.
         </p>
       </div>
     </div>
